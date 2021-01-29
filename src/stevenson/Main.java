97974bcb2de8cb7715b9ee010fba47983c9f9992
Tanner Stratford <57194030+tannerstratford@users.noreply.github.com>
2021-01-28 19:27:53 -0700

    Tread carefully

--- src/stevenson/Main.java
+++ src/stevenson/Main.java
@@ -3,6 +3,7 @@ package stevenson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello Greg. This is Greg from the future. Stay home tomorrow.");
     }
 }
